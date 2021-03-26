6c24ef94420c5e8de61d37b92c7da52470f3fc6e
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2021-03-26 11:01:21 +0900

    Add code from Lesson 2

---
+++ lecture2/lecture2.jl
@@ -0,0 +1,21 @@
+using BenchmarkTools
+
+
+A = rand(100, 100)
+B = rand(100, 100)
+C = rand(100, 100)
+
+function inner_rows!(C, A, B)
+    for i in 1:100, j in 1:100
+        C[i, j] = A[i, j] + B[i, j]
+    end
+end
+@btime inner_rows!(C, A, B)
+
+function inner_cols!(C, A, B)
+    for j in 1:100, i in 1:100
+        C[i, j] = A[i, j] + B[i, j]
+    end
+end
+@btime inner_cols!(C, A, B)
+
