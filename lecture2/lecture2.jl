ce639f1c3a6f7b8ef6a5b8fae58ec398a99500a2
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2021-03-26 16:21:37 +0900

    Add more code from Lecture 2 notes

--- lecture2/lecture2.jl
+++ lecture2/lecture2.jl
@@ -1,4 +1,5 @@
 using BenchmarkTools
+using StaticArrays
 
 
 A = rand(100, 100)
@@ -18,4 +19,59 @@ function inner_cols!(C, A, B)
     end
 end
 @btime inner_cols!(C, A, B)
+
+typeof(A)
+
+function inner_alloc!(C, A, B)
+    for j in 1:100, i in 1:100
+        val = [A[i, j] + B[i, j]]
+        C[i, j] = val[1]
+    end
+end
+@btime inner_alloc!(C, A, B)
+
+function inner_noalloc!(C, A, B)
+    for j in 1:100, i in 1:100
+        val = A[i, j] + B[i, j]
+        C[i, j] = val[1]
+    end
+end
+@btime inner_noalloc!(C, A, B)
+
+
+val = SVector{3, Float64}(1., 2., 3.)
+typeof(val)
+
+function static_inner_alloc!(C, A, B)
+    for j in 1:100, i in 1:100
+        val = @SVector [A[i, j] + B[i, j]]
+        C[i, j] = val[1]
+    end
+end
+@btime static_inner_alloc!(C, A, B)
+
+function inner_alloc(A, B)
+    C = similar(A)
+    for j in 1:100, i in 1:100
+        val = A[i, j] + B[i, j]
+        C[i, j] = val[1]
+    end
+end
+@btime inner_alloc(A, B)
+
+
+function f(A, B)
+    sum([A + B for k in 1:10])
+end
+@btime f(A, B)
+
+# Alternative implementation to f above
+function g(A, B)
+    C = similar(A)
+    for k in 1:10
+        C .+= A .+ B
+    end
+    C
+end
+@btime g(A, B)
 
